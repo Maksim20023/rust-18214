25cd0efcb46d7ca03efa5c5be1432a8c5ce9bee3
maksim.semenov <maksim.semenov@jetbrains.com>
2025-07-14 13:13:42 +0200

    second commit

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -7,10 +7,12 @@ struct Baz;
 do_something! {
     foo: Foo,
     bar: Bar,
+    cas: Cas,
+    baz: Baz,
 }
 
 fn foo() {
-    let _d = Data::new(Foo, Bar);
+    let _d = Data::new(Foo, Bar, Cas, Baz);
 }
 
 fn main() {foo()}
