9793430284c02967669f8b979b80f84083985536
maksim.semenov <maksim.semenov@jetbrains.com>
2025-07-14 13:18:38 +0200

    third commit

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -7,12 +7,16 @@ struct Baz;
 do_something! {
     foo: Foo,
     bar: Bar,
-    cas: Cas,
-    baz: Baz,
+    //cas: Cas,
+    //baz: Baz,
 }
 
 fn foo() {
-    let _d = Data::new(Foo, Bar, Cas, Baz);
+    let d = Data::new(Foo, Bar);//, Cas, Baz);
+    d.foo;
+    d.bar;
+    //d.cas;
+    //d.baz;
 }
 
 fn main() {foo()}
